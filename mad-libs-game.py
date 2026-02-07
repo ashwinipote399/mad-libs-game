af70a3a859d5ebe16b1fa9f3d686284bcdfccddf
Ashwini Anil  Pote <96345207+ashwinipote399@users.noreply.github.com>
2026-02-07 15:01:38 +0530

    Add files via upload

---
+++ mad-libs-game.py
@@ -0,0 +1,17 @@
+# Mad Libs Game using Sense Words
+
+color = input("Enter a color: ")
+sound = input("Enter a sound: ")
+smell = input("Enter a smell: ")
+taste = input("Enter a taste: ")
+feeling = input("Enter a feeling: ")
+place = input("Enter a place: ")
+
+print("\n--- Your Mad Libs Story ---\n")
+
+print("Today I went to the", place + ".")
+print("I saw a", color, "object.")
+print("I heard a", sound + ".")
+print("The place smelled like", smell + ".")
+print("I tasted something", taste + ".")
+print("It made me feel very", feeling + ".")
